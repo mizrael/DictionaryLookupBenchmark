28a415c69265e32a6a064824d2db1964e37934d1
David Guida <1432872+mizrael@users.noreply.github.com>
2024-11-18 15:03:42 -0500

    added dictionary lookup benchmark

---
+++ DictionaryLookupBenchmark/DictionaryLookupBenchmark.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+
+[SimpleJob(runtimeMoniker: RuntimeMoniker.Net80)]
+[SimpleJob(runtimeMoniker: RuntimeMoniker.Net90)]
+public class DictionaryLookupBenchmark
+{
+    private Dictionary<int, string> _dict;
+    private readonly Consumer _consumer = new();
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _dict = Enumerable.Range(0, this.Length)
+                   .ToDictionary(x => x, x => x.ToString());
+    }
+
+    [Benchmark(Baseline = true)]
+    public void TryGetValue()
+    {
+        for (int i = 0; i != this.Length; i++)
+        {
+            if (_dict.TryGetValue(i, out var value))
+            {
+                _consumer.Consume(value);
+            }
+        }
+    }
+
+    [Benchmark()]
+    public void LookupAndGet()
+    {
+        for (int i = 0; i != this.Length; i++)
+        {
+            if (_dict.ContainsKey(i))
+            {
+                var value = _dict[i];
+                _consumer.Consume(value);
+            }
+        }
+    }
+
+    [Params(1000, 10_000, 100_000)]
+    public int Length { get; set; }
+}
